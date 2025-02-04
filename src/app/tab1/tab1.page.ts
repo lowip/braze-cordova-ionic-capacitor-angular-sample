a300980e21ea1536e53821694895b8cdf701fe65
Louis Bur <louisbur92@gmail.com>
2025-02-04 11:49:07 +0100

    Example Braze integration

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// Import the BrazePlugin module, we use the `// @ts-ignore` comment below to ignore the TypeScript error
+// @ts-ignore
+import * as BrazePlugin from 'braze-cordova-sdk';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -11,7 +15,11 @@ export class Tab1Page {
   constructor() {}
 
   logCustomEvent() {
-    console.log("log custom event")
+    // Example of use of the BrazePlugin API
+    // See:
+    // - BrazePlugin symbols: https://github.com/braze-inc/braze-cordova-sdk/blob/master/www/BrazePlugin.js
+    // - Example usage: https://github.com/braze-inc/braze-cordova-sdk/blob/master/sample-project/www/js/index.js
+    BrazePlugin.logCustomEvent("test_event", { "test_key": "test_value" });
   }
 
 }
