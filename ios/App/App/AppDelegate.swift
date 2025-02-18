cc2c9e7d0a62e2b3ca1af7d0f96db88d6b2b51ba
Louis Bur <louisbur92@gmail.com>
2025-02-18 13:47:08 +0100

    Example Braze + @capacitor/push-notifications integration

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -49,4 +49,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    // MARK: - @capacitor/push-notifications implementation
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+
+        // Pass device token to Braze
+        BrazePlugin.braze().notifications.register(deviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
 }
